6d8ffd5743376b598eed13512f15f0ef127371a7
Hamzaoutdoors <ellaouzihamza@gmail.com>
2021-11-19 00:08:50 +0100

    feat : Add Test first to learn TDD pattern

--- spec/solution_spec.rb
+++ spec/solution_spec.rb
@@ -1,58 +1,68 @@
-require_relative '../library/solution'
+require_relative '../library/solver'
 
-describe Solution do
+RSpec.describe Solver do
+  before :all do
+    @solver = Solver.new
+  end
+
+  it 'returns a Solver object' do
+    expect(@solver).to be_an_instance_of(Solver)
+  end
+end
+
+describe '#factorial' do
   before :each do
-    @solution = Solution.new
+    @solver = Solver.new
   end
 
-    it 'returns a Solution object' do
-      expect(@solution).to be_an_instance_of(Solution)
-    end
+  it 'takes parameter and returns a factorial of given number' do
+    expect(@solver.factorial(7)).to eql 5040
   end
 
-  describe '#factorial' do
-    it 'takes parameter and returns a factorial of given number' do
-      expect(@solution.factorial(7)).to eql 5040
-    end
-
-    it 'factorial of 0 is 1' do
-      expect(@solution.factorial(0)).to eql 1
-    end
-
-    it 'raise an exception  if a negative integer is given' do
-      expect { @solution.factorial(-7) }.to raise_error(StandardError)
-    end
+  it 'factorial of 0 is 1' do
+    expect(@solver.factorial(0)).to eql 1
   end
 
-  describe '#reverse' do
-    it 'takes string parameter and returns reversed' do
-      expect(@solution.reverse('Hello')).to eql 'olleH'
-    end
+  it 'raise an exception  if a negative integer is given' do
+    expect { @solver.factorial(-7) }.to raise_error(StandardError)
+  end
+end
 
-    it 'takes string parameter and returns reversed' do
-      expect(@solution.reverse('reverse')).to eql 'esrever'
-    end
-
-    it 'takes string parameter and returns reversed' do
-      expect(@solution.reverse('12345')).to eql '54321'
-    end
+describe '#reverse' do
+  before :each do
+    @solver = Solver.new
   end
 
-  describe '#fizzbuzz' do
-    it 'takes a integer parameter and returns string following given rules' do
-      expect(@solution.fizzbuzz(6)).to eql 'fizz'
-    end
+  it 'takes string parameter and returns reversed' do
+    expect(@solver.reverse('Hello')).to eql 'olleH'
+  end
 
-    it 'takes a integer parameter and returns string following given rules' do
-      expect(@solution.fizzbuzz(10)).to eql 'buzz'
-    end
+  it 'takes string parameter and returns reversed' do
+    expect(@solver.reverse('reverse')).to eql 'esrever'
+  end
 
-    it 'takes a integer parameter and returns string following given rules' do
-      expect(@solution.fizzbuzz(15)).to eql 'fizzbuzz'
-    end
+  it 'takes string parameter and returns reversed' do
+    expect(@solver.reverse('12345')).to eql '54321'
+  end
+end
 
-    it 'takes a integer parameter and returns string following given rules' do
-      expect(@solution.fizzbuzz(7)).to eql '7'
-    end
+describe '#fizzbuzz' do
+  before :each do
+    @solver = Solver.new
+  end
+  it 'takes a integer parameter and returns string following given rules' do
+    expect(@solver.fizzbuzz(6)).to eql 'fizz'
+  end
+
+  it 'takes a integer parameter and returns string following given rules' do
+    expect(@solver.fizzbuzz(10)).to eql 'buzz'
+  end
+
+  it 'takes a integer parameter and returns string following given rules' do
+    expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
+  end
+
+  it 'takes a integer parameter and returns string following given rules' do
+    expect(@solver.fizzbuzz(7)).to eql '7'
   end
 end
